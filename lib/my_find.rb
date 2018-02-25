297318a517b0871c17683a7df6fea087f41ff647
Kayla Cross <kac288@georgetown.edu>
2018-02-25 23:31:46 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      collection[i]
+      break
+    end
+    i = i + 1
+  end
 end
